bec0e1822f9450362e611c8689137c2fd09e411b
MohammedBadry <mahamedbadry2003@gmail.com>
2021-05-23 11:09:28 +0200

    Update UserManagementTest.php

--- tests/Unit/UserManagementTest.php
+++ tests/Unit/UserManagementTest.php
@@ -4,24 +4,29 @@ namespace Tests\Unit;
 
 use App\Models\User;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 
 class UserManagementTest extends TestCase
 {
-    //use WithoutMiddleware;
+    use WithFaker;
+
     public function test_admin_can_add_user()
     {
         $admin = User::where('id', 1)->first();
         $usersCount = count(User::all());
+
         $response = $this->actingAs($admin)->post('/users', [
-            'name' => "FExpress",
-            'first_name' => "Ahmed",
-            'last_name' => "Badry",
-            'email' => "abcdef@yahoo.com",
-            'role' => "2",
-            'password' => "123456789",
-            'password_confirmation' => "123456789",
-        ]);
-        //$response->assertOk();
+            'name'                           => Str::random(10),
+            'first_name'                     => $this->faker->firstname,
+            'last_name'                      => $this->faker->lastname,
+            'email'                          => $this->faker->email,
+            'role'                           => "2",
+            'password'                       => 'password',
+            'password_confirmation'          => 'password',
+            'activated'                      => true,
+            'remember_token'                 => Str::random(10),
+            ]);
         $this->assertCount($usersCount+1, User::all());
     }
 }
